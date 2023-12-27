db035afc1884667e428590cd5ce0897801103d6d
Ayshafebin <febinayisha0@gmail.com>
2023-12-27 12:49:33 +0530

    Reverse an array

---
+++ Five.js
@@ -0,0 +1,13 @@
+// Write a function to reverse an array.
+
+// Examples
+// reverse([1, 2, 3, 4]) ➞ [4, 3, 2, 1]
+
+// reverse([9, 9, 2, 3, 4]) ➞ [4, 3, 2, 9, 9]
+
+// reverse([]) ➞ []
+
+const Reverse = (arr) => {
+    return arr.reverse();
+} 
+console.log(Reverse([9, 9, 2, 3, 4]));
