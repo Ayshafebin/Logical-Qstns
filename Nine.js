45d0163ec039f558e68c467cb35060f665b02f65
Ayshafebin <febinayisha0@gmail.com>
2023-12-27 15:50:14 +0530

    Takes an array of numbers or letters and returns a string

---
+++ Nine.js
@@ -0,0 +1,15 @@
+// Create a function that takes an array of numbers or letters and returns a string.
+
+// Examples
+// arrayToString([1, 2, 3, 4, 5, 6]) ➞ "123456"
+
+// arrayToString(["a", "b", "c", "d", "e", "f"]) ➞ "abcdef"
+
+// arrayToString([1, 2, 3, "a", "s", "dAAAA"]) ➞ "123asdAAAA"
+
+const arrayToString = (arr) => {
+   return arr.join('');
+}
+const rslt = arrayToString([1, 2, 3, "a", "s", "dAAAA"]);
+console.log(rslt);
+console.log(typeof rslt);
