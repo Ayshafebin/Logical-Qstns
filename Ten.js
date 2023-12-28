6091aff9a903f9e288fc820a6ac4c5c4ebf30882
Ayshafebin <febinayisha0@gmail.com>
2023-12-28 10:07:28 +0530

    Concatenate two integer arrays

---
+++ Ten.js
@@ -0,0 +1,14 @@
+// Create a function to concatenate two integer arrays.
+
+// Examples
+// concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+
+// concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+
+// concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
+
+const Concat = (arr1,arr2) => {
+    return [...arr1,...arr2];
+    // return arr1.concat(arr2)
+}
+console.log(Concat([1, 3, 5], [2, 6, 8]));
